c0ecdb705775c4a8adfaa7c1d3a8837d9970a06c
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-03-23 15:11:17 -0600

    Create guessGame.py

---
+++ Day 1-5/guessGame.py
@@ -0,0 +1,10 @@
+print("Welcome to the Head and Tails Guessing Game! \n")
+
+import random
+
+guessing_game = random.randint(1, 2)
+
+if guessing_game == 1:
+    print("Heads")
+else:
+    print("Tails")
